3e07fd0d0bee2336c93d9e0d0247421d64c43941
Francesco Buda <francesco.buda3@studio.unibo.it>
2024-10-02 15:06:39 +0200

    first correction for generateConnection.py

--- src/generateConnections.py
+++ src/generateConnections.py
@@ -3,11 +3,15 @@
 
 
 import pandas as pd
+import numpy as np
 from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
 from tqdm import tqdm
 import re
 
+from huggingface_hub import login
+access_token = "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"
+login(access_token)
 
 from src.settings import (
     OUTPUT_DIR,
@@ -29,23 +33,28 @@ llm = LLM(
     gpu_memory_utilization=.95,
     dtype="auto", 
     enforce_eager=True,
-    max_model_len=7000,
+    max_model_len=10000,
     trust_remote_code=True,
 )
 
-data = pd.read_feather(DATA_DIR + "mimiciv_icd10.feather")
+data = pd.read_feather(DATA_DIR + "mimiciv_icd10_entities.feather")
 
-data["tmpPairs"] = []
-data["defPairs"] = []
+data["tmpPairs"] = [set() for _ in range(len(data))]
+data["defPairs"] = [set() for _ in range(len(data))]
+
+goldTranslator = {}
 
 prompts = []
 for index, row in data.iterrows():
-    row["tmpPairs"] = set()
-    row["defPairs"] = set()
     entities = row["entities"]
     golds = row["icd10_diag_titles"]
-    golds_clean = [gold.replace(',','') for gold in golds]
-    goldsJoined = '\n- '.join(golds)    
+    golds_clean = []
+    for gold in golds:
+        clean = gold.replace(',','')
+        if clean not in goldTranslator:
+            goldTranslator[clean] = gold
+        golds_clean.append(clean)
+    goldsJoined = '\n- '.join(golds_clean)    
     n = 10
     groups = [entities[i:i+n] for i in range(0, len(entities), n)]
 
@@ -58,17 +67,17 @@ for index, row in data.iterrows():
             {"role": "user", "content": "These are the medical terms extracted from the note. \n\nTerms:\n"+ groupJoined +"\n\nAssign to each term one of the following label if there is a strong medical connection between them, otherwise just return \"None\": Labels:\n- "+ goldsJoined +"\n\nReturn each pair as a separate item in the following format:\n- term1, label for term1\n- term2, None\n- term3, label for term3\n...\n\nDo not include any additional information."}
         ]
 
-        prompts.append(row["note_id"], prompt)
+        prompts.append((row["note_id"], prompt))
     
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B-Instruct")
 prompts = [(note_id, tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True)) for note_id, prompt in prompts]
 
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 prompts_batched = [prompts[i:i+BATCH_SIZE] for i in range(0, len(prompts), BATCH_SIZE)]
 
 for id_batch, batch in enumerate(tqdm(prompts_batched, desc="Batches processed")):
-    input_prompts = [el[0] for el in batch]
-    ids = [el[1] for el in batch]
+    input_prompts = [el[1] for el in batch]
+    ids = [el[0] for el in batch]
     outputs = llm.generate(input_prompts, sampling_params, use_tqdm=False)
     for i, output in enumerate(outputs):
         cleanEntities = set(data.loc[data["note_id"] == ids[i], "entities"].values)
@@ -82,7 +91,11 @@ for id_batch, batch in enumerate(tqdm(prompts_batched, desc="Batches processed")
                 if (len(splitted2) == 2):
                     term = splitted2[0]
                     label = splitted2[1]
-                    if label != "None" and term in cleanEntities and label in golds:
+                    if label in goldTranslator:
+                        label = goldTranslator[label]
+                    else:
+                        label = "None"
+                    if label != "None" and term in cleanEntities:
                         data.loc[data["note_id"] == ids[i], "tmpPairs"].add((term, label))
 
 
@@ -103,8 +116,8 @@ prompts = [(note_id, tokenizer.apply_chat_template(prompt, tokenize=False, add_g
 prompts_batched = [prompts[i:i+BATCH_SIZE] for i in range(0, len(prompts), BATCH_SIZE)]
 
 for id_batch, batch in enumerate(tqdm(prompts_batched, desc="Batches processed")):
-    input_prompts = [el[0] for el in batch]
-    ids = [el[1] for el in batch]
+    input_prompts = [el[1] for el in batch]
+    ids = [el[0] for el in batch]
     outputs = llm.generate(input_prompts, sampling_params, use_tqdm=False)
     for i, output in enumerate(outputs):
         generated_text = output.outputs[0].text
@@ -118,4 +131,7 @@ for id_batch, batch in enumerate(tqdm(prompts_batched, desc="Batches processed")
                     label = splitted2[1]
                     flag = splitted2[2]
                     if flag == "YES" and (term, label) in data.loc[data["note_id"] == ids[i], "tmpPairs"]:
-                        data.loc[data["note_id"] == ids[i], "defPairs"].add((term, label))
+                        data.loc[data["note_id"] == ids[i], "defPairs"].add((term, goldTranslator[label]))
+
+
+data.to_feather(OUTPUT_DIR + "mimiciv_icd10_pairs.feather")
